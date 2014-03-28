c0302273f161ffeb262c4c1a03bd4c48cbffe3a2
qguo <a.guo.ty@gmail.com>
2014-03-28 03:12:26 -0400

    gui and more sophisticated ai

--- battleship_ai.py
+++ battleship_ai.py
@@ -3,6 +3,7 @@ CS 5100 Proj:   Battleship
 Team:           BigLeg
 Last Modified:  03/21/2014
 '''
+import copy
 import random
 
 MAPSIZE = 10
@@ -72,26 +73,26 @@ class Map:
                 print self.m[i][j],
             print ''
 
-    def mapmax(self):
-        ''' Find the maximum value of the map for agent
-        '''
-        result = 0
-        for i in range(0, self.size):
-            for j in range(0, self.size):
-               if self.m[i][j] > result:
-                   result = self.m[i][j]
-        return result
+    # def mapmax(self):
+    #     ''' Find the maximum value of the map for agent
+    #     '''
+    #     result = 0
+    #     for i in range(0, self.size):
+    #         for j in range(0, self.size):
+    #            if self.m[i][j] > result:
+    #                result = self.m[i][j]
+    #     return result
 
-    def maxcoordinates(self, target):
-        ''' Find the coordinates of the target in the map for agent
-            This and mapmax are used to help AI locate target
-        '''
-        coordinates = []
-        for i in range(0, self.size):
-          for j in range(0, self.size):
-             if self.m[i][j] == target:
-                 coordinates.append((i,j))
-        return random.choice(coordinates)
+    # def maxcoordinates(self, target):
+    #     ''' Find the coordinates of the target in the map for agent
+    #         This and mapmax are used to help AI locate target
+    #     '''
+    #     coordinates = []
+    #     for i in range(0, self.size):
+    #       for j in range(0, self.size):
+    #          if self.m[i][j] == target:
+    #              coordinates.append((i,j))
+    #     return random.choice(coordinates)
 
     def mark_adjacent_coords(self, (x,y), score):
         ''' Hunt-and-Target
@@ -114,7 +115,7 @@ class Map:
     def in_range(self, (x,y)):
         ''' Tell if a coordinate is in map range
         '''
-        if (0 <= x <= self.size) and (0 <= y <= self.size):
+        if (0 <= x < self.size) and (0 <= y < self.size):
             return True
         return False
 
@@ -143,7 +144,9 @@ class Player:
         ''' Attack an enemy coordinate and mark result on
             own enemy map.
         '''
-        self.enemy_map.set((x,y), enemy.hit((x,y)))
+        result = enemy.hit((x,y))
+        self.enemy_map.set((x,y), result)
+        return result
 
     def hit(self, (x,y)):
         ''' Takes a hit from enemy, calculate score
@@ -163,18 +166,280 @@ class Player:
 
 class Agent(Player):
 
+    # target mode parameters
+    direction = None   # -1 - up 1 - down -2 - left 2 - right
+    candidates = [] # direction candidates
+    wrong_direction = False
+    reach_end = False
+    remain_ships = [5, 4, 3, 3, 2]
+    base = None
+    current = None # the cell currently being hit
+    streak = 0
+
+    hunt_mode = True    
+
     def attack(self, enemy, (x,y)):
-        ''' Attack a human coordinate and mark priority score
-            on own enemy map.
+        ''' Attack a coordinate in Hunt-and-Target mode.
         '''
-        if enemy.hit((x,y)) == HIT:
-            self.enemy_map.mark_adjacent_coords((x,y), 2)
+        self.current = (x,y)
+        result = enemy.hit((x,y))
+        if result == HIT:
+            # hit
+            self.streak = self.streak + 1
+            print 'streak', self.streak
+            if self.hunt_mode:
+                # enter target mode
+                self.hunt_mode = False
+                self.base = (x,y)
+            else:
+                # target mode & hit
+                if self.reach_edge():
+                    # if edge is reached, set reach_end = True
+                    self.reach_end = True
+
+        else:
+            # miss
+            print 'miss'
+            if not self.hunt_mode:
+                # target mode: miss
+                if self.streak > 1:
+                    # reach end
+                    self.reach_end = True
+                else:
+                    # wrong direction
+                    self.wrong_direction = True
+
+            # self.enemy_map.mark_adjacent_coords((x,y), 2)
         self.enemy_map.set((x,y), -1)
+        return result
+
+    def reach_edge(self):
+        x, y = self.current
+        direction = self.direction
+        if direction == -1:
+            if y == 0:
+                return True
+        elif direction == 1:
+            if y == MAPSIZE - 1:
+                return True
+        elif direction == -2:
+            if x == 0:
+                return True
+        elif direction == 2:
+            if x == MAPSIZE - 1:
+                return True
+        return False
 
     def find_target(self):
         ''' Find the next target for attack.
         '''
-        return self.enemy_map.maxcoordinates(self.enemy_map.mapmax())
+        if self.hunt_mode:
+            return self.hunt()
+        else:
+            return self.target()
+
+        # return self.choice(self.find_highest_priority())
+
+    def hunt(self):
+        print 'hunt'
+        return self.choice(0)
+
+    def target(self):
+        '''
+        keep track of cell discovered, possible directions, remaining ships
+        and find the best candidate to attack.
+        '''
+        m = self.enemy_map
+        base = self.base
+        next = None
+
+        if self.direction is None:
+            # if direction is not determined yet
+            # find undiscovered spaces around the base.
+            adjacent = m.find_adjacent_by_xy(base)
+            undiscovered_adjacent = [a for a in adjacent if m.get(a) > -1]
+            # find all applicable directions.
+            for u in undiscovered_adjacent:
+                if self.evaluate_direction(u, base):
+                    self.candidates.append(u)
+            
+            # choose one as next direction.
+            chosen = random.choice(self.candidates)
+            self.candidates.remove(chosen)
+            self.direction = self.determine_direction(chosen, base)
+            next = chosen
+            print 'set direction to', self.direction
+
+        elif self.wrong_direction:
+            print 'wrong direction'
+            # direction is wrong, choose another one
+            chosen = random.choice(self.candidates)
+            self.candidates.remove(chosen)
+            self.direction = self.determine_direction(chosen, base)
+            next = chosen
+            self.wrong_direction = False
+
+        elif self.reach_end:
+            print 'reach end'
+            # reached the edge of map, or hit empty space after a streak > 1.
+
+            # if streak reaches max val in remain_ship
+            # sunk a ship. enter hunt mode
+            max_ship = max(self.remain_ships)
+            if self.streak == max_ship:
+                print 'ship', str(max_ship), 'sunk'
+                self.hunt_mode = True
+                self.direction = None
+                self.remain_ships.remove(max_ship)
+                self.streak = 0
+                print 'streak reset to 0'
+                print 'streak', self.streak
+                self.candidates = []
+                self.base = None
+                next = self.hunt()
+                print 'remain ships', self.remain_ships
+            else:
+                # reach end, try the opposite direction
+                # may or may not discover more parts of ship as agent
+                # doesnt know the ship type.
+                self.direction = -self.direction
+
+                chosen = None
+                for c in self.candidates:
+                    if self.determine_direction(c, base) == self.direction:
+                        chosen = c
+
+                if not chosen:
+                    # if no opposite option
+                    # if streak > 1:
+                    # sunk a ship. enter hunt mode
+                    if self.streak > 1:
+                        print 'ship', str(self.streak), 'sunk'
+                        self.hunt_mode = True
+                        self.direction = None
+                        for s in self.remain_ships:
+                            if s == self.streak:
+                                self.remain_ships.remove(s)
+                        self.streak = 0
+                        print 'streak reset to 0'
+                        print 'streak', self.streak
+                        self.candidates = []
+                        self.base = None
+                        next = self.hunt()
+                        print 'remain ships', self.remain_ships
+                    else:
+                        # choose another random direction and continue
+                        chosen = random.choice(self.candidates)
+                        self.candidates.remove(chosen)
+                        self.direction = self.determine_direction(chosen, base)
+                        next = chosen
+                        print 'reset direction to', self.direction
+                else:
+                    # pursue the opposite direction
+                    self.candidates.remove(chosen)
+                    next = chosen
+                    print 'go to opposite', self.direction
+
+            self.reach_end = False
+
+        else:
+            # if everything alright, continue
+            next = self.moveon()
+            print 'move on direction', self.direction
+            # remove all candidates except the opposite
+            for c in self.candidates:
+                if self.determine_direction(c, base) != -self.direction:
+                    self.candidates.remove(c)
+
+        return next
+
+
+    def determine_direction(self, (x,y), base):
+        bx, by = base
+        if y == by - 1:
+            return -1
+        elif y == by + 1:
+            return 1
+        elif x == bx - 1:
+            return -2
+        elif x == bx + 1:
+            return 2
+        return None
+
+    def evaluate_direction(self, (x,y), base):
+        bx, by = base
+        direction = self.determine_direction((x,y), base)
+        for ship in self.remain_ships:
+            valid = self.have_continous_cells(direction, base, ship, MAPSIZE)
+            if valid:
+                return True
+        return False
+
+
+    def have_continous_cells(self, direction, base, length, size):
+        bx, by = base
+        m = self.enemy_map
+        if direction == -1:
+            if by > length - 2:
+                discovered = [i for i in range(by + 1 - length, by) if m.get((bx,i)) == -1]
+                if not discovered:
+                    return True
+        elif direction == 1:
+            if by < size - length + 1:
+                discovered = [i for i in range(by + 1, by + length) if m.get((bx,i)) == -1]
+                if not discovered:
+                    return True
+        elif direction == -2:
+            if bx > length - 2:
+                discovered = [i for i in range(bx + 1 - length, bx) if m.get((i,by)) == -1]
+                if not discovered:
+                    return True
+        elif direction == 2:
+            if bx < size - length + 1:
+                discovered = [i for i in range(bx + 1, bx + length) if m.get((i,by)) == -1]
+                if not discovered:
+                    return True
+
+        return False
+
+    def moveon(self):
+        direction = self.direction
+        x, y = self.current
+        if direction == -1: # up
+            return (x, y - 1)
+        elif direction == 1: # down
+            return (x, y + 1)
+        elif direction == -2:   # left
+            return (x - 1, y)
+        elif direction == 2:    # right
+            return (x + 1, y)
+        return None
+
+
+    def find_highest_priority(self):
+        ''' Find the maximum value of the map for agent
+        '''
+        size = self.enemy_map.size
+        m = self.enemy_map.m
+        result = 0
+        for i in range(0, size):
+            for j in range(0, size):
+               if m[i][j] > result:
+                   result = m[i][j]
+        return result
+
+    def choice(self, highest):
+        ''' Choose the coordinates of the target in the map for agent
+            This and find_highest_priority are used to help AI locate target
+        '''
+        size = self.enemy_map.size
+        m = self.enemy_map.m
+        candidates = []
+        for i in range(0, size):
+          for j in range(0, size):
+             if m[i][j] == highest:
+                 candidates.append((i,j))
+        return random.choice(candidates)
 
 
 class Human(Player):
@@ -202,6 +467,16 @@ def init_agent(mapsize, arrangement, health):
     enemy_map = Map(size=mapsize)
     enemy_map.generate_ai_enemy_map()
     return Agent(my_map=my_map, enemy_map=enemy_map, health=health)
+
+def array_to_arrangement(array):
+    ''' Convert two-dimension array to arrangement.
+    '''
+    arrangement = []
+    for i in range(0, MAPSIZE):
+        for j in range(0, MAPSIZE):
+            if array[i][j]:
+                arrangement.append((i,j))
+    return arrangement
 
 
 if __name__ == '__main__':
--- battleship_gui.py
+++ battleship_gui.py
@@ -1,17 +1,22 @@
 
-import random, pygame, sys
+import random, pygame, sys, time
 from pygame.locals import *
 
-FPS = 10
-WINDOWWIDTH = 640
-WINDOWHEIGHT = 480
-CELLSIZE = 20
-assert WINDOWWIDTH % CELLSIZE == 0  
-assert WINDOWHEIGHT % CELLSIZE == 0 
+from battleship_ai import Map, Agent, Human, init_agent, init_human, array_to_arrangement
+from battleship_ai import HEALTH, TEST_ARRANGEMENT
+
+FPS = 30
+WINDOWWIDTH = 1065
+WINDOWHEIGHT = 555
+CELLSIZE = 48
+# assert WINDOWWIDTH % CELLSIZE == 0
+# assert WINDOWHEIGHT % CELLSIZE == 0
 CELLWIDTH = int(WINDOWWIDTH / CELLSIZE)
 CELLHEIGHT = int(WINDOWHEIGHT / CELLSIZE)
+BOARDSIZE = 10  # 10x10 board
+MARGIN = 5
 
-# Set the colors
+# Define colors
 #             R    G    B
 WHITE     = (255, 255, 255)
 BLACK     = ( 0,   0,   0)
@@ -20,15 +25,23 @@ BLUE      = ( 0,   0,  255)
 DARKGREEN = ( 0,  155,  0)
 GREEN     = ( 0,  255,  0)
 DARKGRAY  = ( 40,  40, 40)
+LIGHTBLUE = (135, 206, 250)
 BGCOLOR   = BLUE
+NAVY = (0, 0, 128)
+
+SHIPS = [5,4,3,3,2]
+
+
+# boardImage = pygame.image.load('bgi.jpg')
 
 def main():
-    global FPSCLOCK, DISPLAYSURF, BASICFONT
+    global FPSCLOCK, DISPLAYSURF, BASICFONT, SMALLFONT, GRID
 
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
+    SMALLFONT = pygame.font.Font('freesansbold.ttf', 12)
 
     mousex = 0  # store the x coordinate of mouse event
     mousey = 0  # store the y coordinate of mouse event
@@ -36,40 +49,151 @@ def main():
 
     firstSelection = None
 
-    pygame.mixer.music.load('bgm.mp3') # load the background music
-    pygame.mixer.music.play(-1, 0.0)
+    # pygame.mixer.music.load('bgm.mp3') # load the background music
+    # pygame.mixer.music.play(-1, 0.0)
+
+    GRID = init_grid()
+
     showStartScreen()
     
-    while True:
-        runGame()
+    arrangement()
         #showGameOverScreen() -- need to be implement
+    game()
     pygame.mixer.music.stop
                 
-def runGame():
+def arrangement():
     mouseClicked = False
     DISPLAYSURF.fill(BGCOLOR)
+
     drawGrid()
-    for event in pygame.event.get():
-        if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
-            pygame.quit()
-            sys.exit()
-        elif event.type == MOUSEMOTION:
-            mousex, mousey = event.pos
-        elif event.type == MOUSEBUTTONUP:
-            mousex, mousey = event.pos
-            mouseClicked = True
-            
+
+    while SHIPS:
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit(0)
+            # elif event.type == MOUSEMOTION:
+            #     mousex, mousey = event.pos
+            # elif event.type == MOUSEBUTTONUP:
+            #     mousex, mousey = event.pos
+            #     mouseClicked = True
+            elif event.type == MOUSEBUTTONDOWN:
+                # mark selected positions
+                pos = event.pos
+                x = pos[0] // (CELLSIZE + MARGIN)
+                y = pos[1] // (CELLSIZE + MARGIN)
+                if x < BOARDSIZE:  # only place ship on human side
+                    place_ship(x, y)
+                    # grid1[row][column] = 1
+                # else:
+                #     grid2[row][column - BOARDSIZE] = 1
+
+        drawGrid()
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
 #    shipx, shipy = getShipAtPixel(mousex, mousey)
 #    if shipx != None and shipy != None:
 # ---- Need to implement the content while playing the game 
-    pygame.display.update()
-    FPSCLOCK.tick(FPS)
+        
+
+def game():
+    human_turn = True
+    human = init_human(BOARDSIZE, array_to_arrangement(GRID), HEALTH)
+    agent = init_agent(BOARDSIZE, TEST_ARRANGEMENT, HEALTH)
+    x = 0
+    y = 0
+
+    while True:
+        drawScore(human.health, agent.health, human_turn)
+        pygame.display.update()
+
+        if human_turn:
+            # wait for a choice
+            for event in pygame.event.get():
+                if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+                    pygame.quit()
+                    sys.exit(0)
+                elif event.type == MOUSEBUTTONDOWN:
+                    pos = event.pos
+                    x = pos[0] // (CELLSIZE + MARGIN)
+                    y = pos[1] // (CELLSIZE + MARGIN)
+
+            if x >= BOARDSIZE:
+                # mark selected cell as candidate
+                GRID[x][y] = 'C'
+
+                drawGrid()
+                pygame.display.update()
+
+                pygame.time.wait(500)
+
+                result = human.attack(agent, (x - BOARDSIZE,y))
+                GRID[x][y] = result
+
+                drawGrid()
+                pygame.display.update()
+
+                if result == 'H':
+                    pygame.time.wait(500)
+                else:
+                    human_turn = not human_turn
+
+                GRID[x][y] = '0'
+                drawGrid()
+                pygame.display.update()
+
+                x = 0
+                y = 0
+        else:
+            pygame.time.wait(500)
+
+            x, y = agent.find_target()
+            GRID[x][y] = 'C'
+
+            drawGrid()
+            pygame.display.update()
+
+            pygame.time.wait(500)
+
+            result = agent.attack(human, (x, y))
+            GRID[x][y] = result
+
+            drawGrid()
+            pygame.display.update()
+            
+
+            if result == 'H':
+                pygame.time.wait(500)
+            else:
+                human_turn = not human_turn
+
+            GRID[x][y] = '0'
+            drawGrid()
+            pygame.display.update()
+
+            x = 0
+            y = 0
+
+        FPSCLOCK.tick(FPS)
+
+
+    # human firstSelection
 
 def drawPressKeyMsg():
     pressKeySurf = BASICFONT.render('Press any key to play.', True, DARKGRAY)
     pressKeyRect = pressKeySurf.get_rect()
     pressKeyRect.topleft = (WINDOWWIDTH - 200, WINDOWHEIGHT - 30)
     DISPLAYSURF.blit(pressKeySurf, pressKeyRect)
+
+def drawMsg(msg, (x, y)):
+    word = BASICFONT.render(msg, True, WHITE)
+    rect = word.get_rect()
+    rect.topleft = (x, y)
+    # clear that area first
+    DISPLAYSURF.fill(BGCOLOR, rect)
+    DISPLAYSURF.blit(word, rect)
+
 
 def checkForKeyPress():
     if len(pygame.event.get(QUIT)) > 0:
@@ -108,8 +232,8 @@ def showStartScreen():
             return
         pygame.display.update()
         FPSCLOCK.tick(FPS)
-        degrees1 += 3 
-        degrees2 += 7 
+        degrees1 += 1
+        degrees2 += 3
 '''
 def getShipAtPixel(mousex, mousey)
 '''
@@ -118,11 +242,92 @@ def terminate():
     pygame.quit()
     sys.exit()
 
+def init_grid():
+    return [[0 for y in range(BOARDSIZE)] for y in range(BOARDSIZE * 2)]
+
+def drawScore(human_score, agent_score, human_turn):
+    # erase previous msgs
+    rect = pygame.Rect(0, WINDOWHEIGHT - 20, WINDOWWIDTH, 20)
+    DISPLAYSURF.fill(BGCOLOR, rect)
+
+    drawMsg('Score: ' + str(human_score), (20, WINDOWHEIGHT - 20))
+    drawMsg('Score: ' + str(agent_score), (WINDOWWIDTH - 100, WINDOWHEIGHT - 20))
+    if human_turn:
+        drawMsg('Your turn', (WINDOWWIDTH / 2 - 40, WINDOWHEIGHT - 20))
+    else:
+        drawMsg('AI turn', (WINDOWWIDTH / 2 - 40, WINDOWHEIGHT - 20))
+
 def drawGrid():
-    for x in range(0, WINDOWWIDTH, CELLSIZE): 
-        pygame.draw.line(DISPLAYSURF, DARKGRAY, (x, 0), (x, WINDOWHEIGHT))
-    for y in range(0, WINDOWHEIGHT, CELLSIZE): 
-        pygame.draw.line(DISPLAYSURF, DARKGRAY, (0, y), (WINDOWWIDTH, y))
+    # for x in range(0, WINDOWWIDTH, CELLSIZE): 
+    #     pygame.draw.line(DISPLAYSURF, DARKGRAY, (x, 0), (x, WINDOWHEIGHT))
+    # for y in range(0, WINDOWHEIGHT, CELLSIZE): 
+    #     pygame.draw.line(DISPLAYSURF, DARKGRAY, (0, y), (WINDOWWIDTH, y))
+
+    # draw a line in the middle
+    pygame.draw.line(DISPLAYSURF, NAVY, (WINDOWWIDTH / 2, 5), (WINDOWWIDTH / 2, WINDOWHEIGHT - 25), 5)
+
+    for x in range(BOARDSIZE):
+        for y in range(BOARDSIZE):
+            color = WHITE
+            if GRID[x][y] == 1:
+                color = GREEN
+            elif GRID[x][y] == '0':
+                color = WHITE
+            elif GRID[x][y] == 'M':
+                color = WHITE
+            elif GRID[x][y] == 'H':
+                color = RED
+            pygame.draw.rect(DISPLAYSURF,
+                             color,
+                             [(MARGIN + CELLSIZE) * x + MARGIN,
+                              (MARGIN + CELLSIZE) * y + MARGIN,
+                              CELLSIZE,
+                              CELLSIZE])
+            if GRID[x][y] == 'C':
+                pygame.draw.circle(DISPLAYSURF,
+                                NAVY,
+                                ((MARGIN + CELLSIZE) * x + MARGIN + 24, (MARGIN + CELLSIZE) * y + MARGIN + 24),
+                                24,
+                                2)
+
+    for x in range(BOARDSIZE, 2 * BOARDSIZE):
+        for y in range(BOARDSIZE):
+            color = LIGHTBLUE
+            if GRID[x][y] == '0':
+                color = WHITE
+            elif GRID[x][y] == 'M':
+                color = WHITE
+            elif GRID[x][y] == 'H':
+                color = RED
+            pygame.draw.rect(DISPLAYSURF,
+                             color,
+                             [(MARGIN + CELLSIZE) * x + MARGIN,
+                              (MARGIN + CELLSIZE) * y + MARGIN,
+                              CELLSIZE,
+                              CELLSIZE])
+            if GRID[x][y] == 'C':
+                pygame.draw.circle(DISPLAYSURF,
+                                WHITE,
+                                ((MARGIN + CELLSIZE) * x + MARGIN + 24, (MARGIN + CELLSIZE) * y + MARGIN + 24),
+                                24,
+                                2)
+
+
+def place_ship(x, y):
+    if SHIPS:
+        crnt_ship = SHIPS[0]
+        # place ship based on x and y
+        GRID[x][y] = 1
+        if x <= BOARDSIZE - crnt_ship:
+            # place horizontally
+            for i in range(x, x + crnt_ship):
+                GRID[i][y] = 1
+        else:
+            # not enough space, stick to right edge
+            for i in range(BOARDSIZE - crnt_ship, BOARDSIZE):
+                GRID[i][y] = 1
+
+        del SHIPS[0]
 
 
 if __name__ == '__main__':
