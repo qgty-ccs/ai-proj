f3161c45ab8840abb63c9239040001c5ec213559
qguo <a.guo.ty@gmail.com>
2014-04-11 02:18:30 -0400

    added time benchmark

--- ai-tester.py
+++ ai-tester.py
@@ -17,8 +17,12 @@ AI1_MISS = 0
 AI1_HIT = 0
 AI2_MISS = 0
 AI2_HIT = 0
-AI1_WIN_COUNTER = 0
-AI2_WIN_COUNTER = 0
+AI1_TOTAL_WIN_STEPS = 0
+AI2_TOTAL_WIN_STEPS = 0
+AI1_TOTAL_STEPS = 0
+AI2_TOTAL_STEPS = 0
+AI1_TOTAL_THINKING_TIME = 0
+AI2_TOTAL_THINKING_TIME = 0
 
 def run(ai1, ai2):
     global  AI1_WINS,\
@@ -27,24 +31,37 @@ def run(ai1, ai2):
             AI1_HIT,\
             AI2_MISS,\
             AI2_HIT,\
-            AI1_WIN_COUNTER,\
-            AI2_WIN_COUNTER
+            AI1_TOTAL_WIN_STEPS,\
+            AI2_TOTAL_WIN_STEPS,\
+            AI1_TOTAL_STEPS,\
+            AI2_TOTAL_STEPS,\
+            AI1_TOTAL_THINKING_TIME,\
+            AI2_TOTAL_THINKING_TIME
 
     counter = 0
+    elapsed = 0
     ai1_turn = random.choice([True, False])
 
     while True:
         if ai1.lose():
             AI2_WINS = AI2_WINS + 1
-            AI2_WIN_COUNTER = AI2_WIN_COUNTER + counter
+            AI2_TOTAL_WIN_STEPS = AI2_TOTAL_WIN_STEPS + counter
             break
         if ai2.lose():
             AI1_WINS = AI1_WINS + 1
-            AI1_WIN_COUNTER = AI1_WIN_COUNTER + counter
+            AI1_TOTAL_WIN_STEPS = AI1_TOTAL_WIN_STEPS + counter
             break
 
         if ai1_turn:
-            r = ai1.attack(ai2, ai1.find_target())
+            start = time.clock()
+            # evaluate decision time
+            target = ai1.find_target()
+            elapsed = (time.clock() - start)
+
+            AI1_TOTAL_STEPS = AI1_TOTAL_STEPS + 1
+            AI1_TOTAL_THINKING_TIME = AI1_TOTAL_THINKING_TIME + elapsed
+
+            r = ai1.attack(ai2, target)
             result = r['result']
             if result == 'M':
                 AI1_MISS = AI1_MISS + 1
@@ -52,7 +69,15 @@ def run(ai1, ai2):
             if result == 'H':
                 AI1_HIT = AI1_HIT + 1
         else:
-            r = ai2.attack(ai1, ai2.find_target())
+            start = time.clock()
+            # evaluate decision time
+            target = ai2.find_target()
+            elapsed = (time.clock() - start)
+
+            AI2_TOTAL_STEPS = AI2_TOTAL_STEPS + 1
+            AI2_TOTAL_THINKING_TIME = AI2_TOTAL_THINKING_TIME + elapsed
+
+            r = ai2.attack(ai1, target)
             result = r['result']
             if result == 'M':
                 AI2_MISS = AI2_MISS + 1
@@ -64,25 +89,31 @@ def run(ai1, ai2):
 
 def summary():
     global  AI1_WINS,\
-        AI2_WINS,\
-        AI1_MISS,\
-        AI1_HIT,\
-        AI2_MISS,\
-        AI2_HIT,\
-        AI1_WIN_COUNTER,\
-        AI2_WIN_COUNTER
+            AI2_WINS,\
+            AI1_MISS,\
+            AI1_HIT,\
+            AI2_MISS,\
+            AI2_HIT,\
+            AI1_TOTAL_WIN_STEPS,\
+            AI2_TOTAL_WIN_STEPS,\
+            AI1_TOTAL_STEPS,\
+            AI2_TOTAL_STEPS,\
+            AI1_TOTAL_THINKING_TIME,\
+            AI2_TOTAL_THINKING_TIME
 
     print '**********'
     print 'AI 1'
-    print 'Winning Percentage:', AI1_WINS * 100 / float(ROUND), '%'
-    print 'Hits/Shots Fired Percentage:', AI1_HIT * 100 / float(AI1_HIT + AI1_MISS), '%'
-    print 'Avg Win in Turns:', AI1_WIN_COUNTER / float(ROUND)
+    print 'Winning Percentage:',            AI1_WINS * 100 / float(ROUND), '%'
+    print 'Hits/Shots Fired Percentage:',   AI1_HIT * 100 / float(AI1_HIT + AI1_MISS), '%'
+    print 'Avg Steps to Win:',              AI1_TOTAL_WIN_STEPS / float(ROUND)
+    print 'Avg Thinking Time:',             AI1_TOTAL_THINKING_TIME * 1000 / float(AI1_TOTAL_STEPS)
 
     print '**********'
     print 'AI 2'
-    print 'Winning Percentage:', AI2_WINS * 100 / float(ROUND), '%'
-    print 'Hits/Shots Fired Percentage:', AI2_HIT * 100 / float(AI2_HIT + AI2_MISS), '%'
-    print 'Avg Win in Turns:', AI2_WIN_COUNTER / float(ROUND)
+    print 'Winning Percentage:',            AI2_WINS * 100 / float(ROUND), '%'
+    print 'Hits/Shots Fired Percentage:',   AI2_HIT * 100 / float(AI2_HIT + AI2_MISS), '%'
+    print 'Avg Steps to Win:',              AI2_TOTAL_WIN_STEPS / float(ROUND)
+    print 'Avg Thinking Time:',             AI2_TOTAL_THINKING_TIME * 1000 / float(AI2_TOTAL_STEPS)
 
 def init_ai(ai_class):
     my_map = Map(size=MAPSIZE)
